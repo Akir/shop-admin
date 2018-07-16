845dfedfeea5ddc45c1feaa26869c14767cb07ab
Akiran <787685485@qq.com>
2018-07-16 14:26:47 +0800

    基础配置

---
+++ src/main/java/shop/config/AppConfig.java
@@ -0,0 +1,70 @@
+package shop.config;
+
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("shop")
+@MapperScan("shop.mapper")
+@PropertySource("classpath:shop/jdbc.properties")
+@EnableTransactionManagement
+public class AppConfig extends WebMvcConfigurationSupport {
+
+	@Override
+	protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	protected void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/jsp/", ".jsp");
+	}
+	
+	@Bean
+	public DataSource dataSource(Environment en) {
+		DriverManagerDataSource ds = new DriverManagerDataSource(
+				en.getProperty("jdbc.url"), 
+				en.getProperty("jdbc.username"), 
+				en.getProperty("jdbc.password"));
+		ds.setDriverClassName(en.getProperty("jdbc.driverClassName"));
+		return ds;
+	}
+	
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource ds) {
+		SqlSessionFactoryBean sf = new SqlSessionFactoryBean();
+		sf.setConfigLocation(new ClassPathResource("shop/mybatis-comfig.xml"));
+		sf.setDataSource(ds);
+		return sf;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource ds) {
+		return new DataSourceTransactionManager(ds);
+	}
+	
+}
