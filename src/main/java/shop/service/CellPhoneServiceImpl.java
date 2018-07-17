9b4cfaf0585cdfc6bf3838824071f45c42b06dba
Akiran <787685485@qq.com>
2018-07-17 16:22:22 +0800

    Spring事务

--- src/main/java/shop/service/CellPhoneServiceImpl.java
+++ src/main/java/shop/service/CellPhoneServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import shop.mapper.CellPhoneMapper;
 import shop.model.CellPhone;
 
 @Service
+@Transactional
 public class CellPhoneServiceImpl implements CellPhoneService {
 	private CellPhoneMapper cellPhoneMapper;
 
