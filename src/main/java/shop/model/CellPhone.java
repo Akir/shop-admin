7fc92e18be94094b0ad4e3f54a93a5d30712a661
Akiran <787685485@qq.com>
2018-07-17 11:19:30 +0800

    创建数据库映射实体--CellPhone和Brand

---
+++ src/main/java/shop/model/CellPhone.java
@@ -0,0 +1,74 @@
+package shop.model;
+
+public class CellPhone {
+	private long id;
+	private String model;
+	private String os;
+	private String cpu;
+	private int ram;
+	private int storage;
+	private String color;
+	private int price;
+	private String description;
+	private Brand brand;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getOs() {
+		return os;
+	}
+	public void setOs(String os) {
+		this.os = os;
+	}
+	public String getCpu() {
+		return cpu;
+	}
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+	public int getRam() {
+		return ram;
+	}
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+	public int getStorage() {
+		return storage;
+	}
+	public void setStorage(int storage) {
+		this.storage = storage;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Brand getBrand() {
+		return brand;
+	}
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+}
