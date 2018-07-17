a0c5348b89b815573ef95758eaf60fad4c93040a
Akiran <787685485@qq.com>
2018-07-17 10:14:59 +0800

    SQL--手机表的创建

--- scripts/db/init-db.sql
+++ scripts/db/init-db.sql
@@ -5,7 +5,18 @@ create table brand(
        brand_name varchar2(64) not null,
        brand_description varchar2(1000)
 );
-
+create table cellphone(
+       cellphone_id number primary key,
+       cellphone_model varchar2(64) not null,
+       cellphone_os varchar2(64),
+       cellphone_cpu varchar2(64),
+       cellphone_ram number,            --单位为GB
+       cellphone_storage number,        --单位为GB
+       cellphone_color varchar2(64),
+       cellphone_price number not null, --单位为分
+       brand_id number references brand(brand_id) not null,
+       cellphone_description varchar2(1000)
+);
 
 
 
