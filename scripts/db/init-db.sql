ab91ac62173ad4ccccc9449559610903a4b17e61
Akiran <787685485@qq.com>
2018-07-17 10:00:33 +0800

    SQL--手机品牌厂商表的创建

--- scripts/db/init-db.sql
+++ scripts/db/init-db.sql
@@ -1,2 +1,15 @@
 create user phone identified by 123456;
 grant connect, resource to phone;
+create table brand(
+       brand_id number primary key,
+       brand_name varchar2(64) not null,
+       brand_description varchar2(1000)
+);
+
+
+
+
+
+
+
+
